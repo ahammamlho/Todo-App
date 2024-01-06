23e349b5b5e50615832fb5a4b372383f24a58828
AHAMMAM LHOUSSAINE <ahammamlho96@gmail.com>
2024-01-06 16:51:04 +0100

    delete .dist

--- backend/dist/todo/dto/todo.dto.d.ts
+++
@@ -1,3 +0,0 @@
-export declare class TodoDto {
-    todo: string;
-}
--- frontend/.next/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"fxkvYdKSwI5DSS6Ifxcdwxx9LLzGgmMwyDQfkHh5OaA=\"\n}"
